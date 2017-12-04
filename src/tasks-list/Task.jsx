cf55e30e5aa5a1ca70a6bed9ed58a7c11f9f84a4
alenadmit <dmitalyona@yandex.ru>
2017-12-04 23:25:35 +0300

    label checkbox doesn't work

--- src/tasks-list/Task.jsx
+++ src/tasks-list/Task.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './Task.css';
 
+let idForCheckbox = 0;
+
 export default class Task extends Component{
     // constructor(props){
     //     super(props);
     // }
-
     render(){
+        // let idForCheckbox = getIdForCheckbox();
         return(
             <div className="task-container">
                 <div>
-                    <input id="check" type="checkbox"/>
-                    <label className="task-title" for="check">{this.props.title}</label>
+                    <input id={`check${this.props.id}`} type="checkbox"/>
+                    <label className="task-title" for={`check${this.props.id}`}>{this.props.title}</label>
                 </div>
                 <div className="task-info">
                     <div className="task-deadline">{this.props.deadline}</div>
@@ -19,6 +21,9 @@ export default class Task extends Component{
                 </div>
             </div>
         );
-    }
+    };
 }
 
+const getIdForCheckbox = function () {
+    return ++idForCheckbox;
+};
--- src/tasks-list/TaskList.jsx
+++ src/tasks-list/TaskList.jsx
@@ -8,7 +8,11 @@ export default class TaskList extends Component {
     // }
 
     render() {
-        const listItems = this.props.data.map((item) => <li key={item.id}><Task title={item.title} deadline={item.deadline} done={item.done}/></li>);
+        const listItems = this.props.data.map((item) =>
+            <li key={item.id} >
+                <Task title={item.title} deadline={item.deadline} done={item.done}/>
+            </li>
+        );
         // const listItems = this.props.data.map((item) => <Task {...item} />);
         return (
             <ul className="taskList-container">
